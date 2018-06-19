25f3feda939d7855a575a621ab01f0971b71e58f
masatoshi_moritsuka <yakiyaki_ikayaki@yahoo.co.jp>
2018-06-19 15:50:39 +0900

    検索トップページをデザイン

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,4 +1,9 @@
 import 'src/application'
 import 'bootstrap/dist/js/bootstrap'
+import fontawesome from '@fortawesome/fontawesome'
+import faSearch from '@fortawesome/fontawesome-free-solid/faSearch'
+import faHome from '@fortawesome/fontawesome-free-solid/faHome'
 
 console.log('Hello World from Webpacker')
+fontawesome.library.add(faSearch);
+fontawesome.library.add(faHome);
