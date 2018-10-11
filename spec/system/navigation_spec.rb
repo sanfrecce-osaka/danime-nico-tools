45ad2bbbed8490d25d37663c063b9dc8391e4871
masatoshi_moritsuka <yakiyaki_ikayaki@yahoo.co.jp>
2018-10-11 21:19:31 +0900

    ナビゲーションのリンクに関するspecを追加
    - current-nav-linkが正常に付加されているかどうかのspcを追加
    - RSpecの設定周りを修正

---
+++ spec/system/navigation_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.feature 'Navigation', type: :system do
+  scenario 'ナビゲーションの適切なリンクのクラスにcurrrent-nav-linkが付加されている', js: true do
+    visit root_path
+    expect(find('nav').all('li')[0].has_css?('.current-nav-link')).to be_truthy
+    expect(find('nav').all('li')[1].has_no_css?('.current-nav-link')).to be_truthy
+
+    visit animes_path
+    expect(find('nav').all('li')[0].has_no_css?('.current-nav-link')).to be_truthy
+    expect(find('nav').all('li')[1].has_css?('.current-nav-link')).to be_truthy
+  end
+end
