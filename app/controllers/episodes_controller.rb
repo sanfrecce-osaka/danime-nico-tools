cdf59a0a506308cb50658238c2b1f207cdd0a9f2
masatoshi_moritsuka <yakiyaki_ikayaki@yahoo.co.jp>
2018-10-18 15:09:36 +0900

    動画検索ページを追加

--- app/controllers/episodes_controller.rb
+++ app/controllers/episodes_controller.rb
@@ -1,5 +1,19 @@
+# frozen_string_literal: true
+
 class EpisodesController < ApplicationController
   def index
+    @episodes =
+      if @search_form.keywords.present?
+        Episode
+          .joins(:season).eager_load(:season)
+          .search_by(@search_form)
+          .order('seasons.title ASC, overall_number ASC')
+          .page(page_params).per(10)
+      else
+        Episode
+          .joins(:season).eager_load(:season)
+          .random(10)
+      end
   end
 
   def show
@@ -8,5 +22,11 @@ class EpisodesController < ApplicationController
       flash[:warning] = '指定された動画は現在公開されていません'
       redirect_to :episodes
     end
+  end
+
+  private
+
+  def page_params
+    params[:page].present? ? params[:page] : 1
   end
 end
