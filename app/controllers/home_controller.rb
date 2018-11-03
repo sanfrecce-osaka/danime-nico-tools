fe1a0d06e107448ca3b7a32acc43f5995f85c1a5
masatoshi_moritsuka <yakiyaki_ikayaki@yahoo.co.jp>
2018-11-03 16:26:40 +0900

    トップページのデザインを修正

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,14 @@
 
 class HomeController < ApplicationController
   def index
+    @seasons = Season.where(watchable: true).random(8)
+    @episodes = Episode.joins(:season).eager_load(:season).where('seasons.watchable = ?', true).random(8)
+    @new_seasons = Season.where(watchable: true).order(updated_at: :desc).limit(8)
+    @new_episodes =
+      Episode
+        .joins(:season).eager_load(:season)
+        .where('seasons.watchable = ?', true)
+        .order('seasons.updated_at desc, episodes.updated_at desc')
+        .limit(8)
   end
 end
