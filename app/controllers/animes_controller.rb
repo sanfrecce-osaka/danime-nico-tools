e6586e5fdee9c275b4f4b4d9fa48c0e5f61f660b
masatoshi_moritsuka <yakiyaki_ikayaki@yahoo.co.jp>
2018-11-05 20:32:35 +0900

    検索ページのデザインを修正

--- app/controllers/animes_controller.rb
+++ app/controllers/animes_controller.rb
@@ -4,17 +4,29 @@ class AnimesController < ApplicationController
   def index
     @seasons =
       if @search_form.keywords.present?
-        Season.search_by(@search_form).limit(5).order(:title)
-      else
-        Season.random(5)
+        if @search_form.category.include_season?
+          Season.search_by(@search_form).order(:title).page(season_page_params).per(12)
+        end
       end
     @episodes =
       if @search_form.keywords.present?
-        Episode.joins(:season).eager_load(:season)
-          .search_by(@search_form).limit(6)
-          .order('seasons.title ASC, overall_number ASC')
-      else
-        Episode.joins(:season).eager_load(:season).random(6)
+        if @search_form.category.include_episode?
+          Episode
+            .joins(:season).eager_load(:season)
+            .search_by(@search_form)
+            .order('seasons.title ASC, overall_number ASC')
+            .page(episode_page_params).per(12)
+        end
       end
+  end
+
+  private
+
+  def season_page_params
+    params[:season_page].present? ? params[:season_page] : 1
+  end
+
+  def episode_page_params
+    params[:episode_page].present? ? params[:episode_page] : 1
   end
 end
