073a74296a7289bd7fefeb267ccee83b91521e0d
masatoshi_moritsuka <yakiyaki_ikayaki@yahoo.co.jp>
2019-02-14 22:32:44 +0900

    作品検索機能を新デザインにあわせて修正

--- app/models/form/season_search.rb
+++ app/models/form/season_search.rb
@@ -4,15 +4,10 @@ module Form
   class SeasonSearch
     include ActiveModel::Model
 
-    attr_accessor :keywords
-    attr_writer :keyword_type
+    attr_accessor :keywords, :keyword_type
 
     def split_keywords
       keywords.split(%r([[:blank:]]))
-    end
-
-    def keyword_type
-      SeasonSearchKeywordType.find_by(type: @keyword_type)
     end
   end
 end
