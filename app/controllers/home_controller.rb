8a78f8742815a27b10ef360c112571587b953478
masatoshi_moritsuka <yakiyaki_ikayaki@yahoo.co.jp>
2019-02-14 19:24:12 +0900

    トップページのデザイン更新に伴い必要なくなったインスタンス変数を削除

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,8 +2,5 @@
 
 class HomeController < ApplicationController
   def index
-    @seasons = Season.where(watchable: true).random(8)
-    @episodes = Episode.joins(:season).eager_load(:season).where('seasons.watchable = ?', true).random(8)
-    @new_seasons = Season.where(watchable: true).order(updated_at: :desc).limit(8)
   end
 end
