bcf8ab7de9fc8cf2b3677504f57ac5b43f130061
masatoshi_moritsuka <yakiyaki_ikayaki@yahoo.co.jp>
2019-02-14 22:32:44 +0900

    作品検索機能を新デザインにあわせて修正

--- app/models/season_search_keyword_type.rb
+++ app/models/season_search_keyword_type.rb
@@ -7,20 +7,4 @@ class SeasonSearchKeywordType < ActiveHash::Base
     { id: 3, type: :staff_name, name: 'スタッフ名' },
     { id: 4, type: :keyword, name: 'キーワード' }
   ]
-
-  def season_title?
-    type == :season_title
-  end
-
-  def cast_name?
-    type == :cast_name
-  end
-
-  def staff_name?
-    type == :staff_name
-  end
-
-  def keyword?
-    type == :keyword
-  end
 end
