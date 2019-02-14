83ae26ba8566c9cd1492c86c85f23329d45ea72d
masatoshi_moritsuka <yakiyaki_ikayaki@yahoo.co.jp>
2019-02-14 22:55:35 +0900

    動画検索機能を削除

--- app/controllers/episodes_controller.rb
+++ app/controllers/episodes_controller.rb
@@ -1,32 +1,11 @@
 # frozen_string_literal: true
 
 class EpisodesController < ApplicationController
-  def index
-    @episodes =
-      if @season_search_form.keywords.present?
-        Episode
-          .joins(:season).eager_load(:season)
-          .search_by(@season_search_form)
-          .order('seasons.title ASC, overall_number ASC')
-          .page(page_params).per(10)
-      else
-        Episode
-          .joins(:season).eager_load(:season)
-          .random(10)
-      end
-  end
-
   def show
     @episode = Episode.joins(:season).eager_load(:season).find_by(content_id: params[:content_id])
     unless @episode.season.watchable
       flash[:warning] = '指定された動画は現在公開されていません'
       redirect_to :episodes
     end
-  end
-
-  private
-
-  def page_params
-    params[:page].present? ? params[:page] : 1
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@
 Rails.application.routes.draw do
   root 'home#index'
   resources :seasons, only: %i(index show)
-  resources :episodes, only: %i(index show), param: :content_id
+  resources :episodes, only: %i(show), param: :content_id
 end
