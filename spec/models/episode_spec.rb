2c5160a2742623d2312aa012930254281eff495b
masatoshi_moritsuka <yakiyaki_ikayaki@yahoo.co.jp>
2019-04-29 17:47:42 +0900

    Episode#full_titleの作品・動画のタイトルが同一かつフラグがtrueで話数が空の場合のテストを追加

--- spec/models/episode_spec.rb
+++ spec/models/episode_spec.rb
@@ -10,4 +10,22 @@ RSpec.describe Episode, type: :model do
       expect(episode.nico_url).to eq 'http://www.nicovideo.jp/watch/so1'
     end
   end
+
+  describe '#full_title' do
+    let(:episode) { create(:episode, :with_season, params) }
+    let(:params) { { title: title, number_in_season: number_in_season } }
+    let(:full_title) { episode.full_title(with_season_title) }
+
+    context '作品タイトルと動画タイトルが同じかつ作品タイトル表示フラグがtrue' do
+      context '話数が空' do
+        let(:title) { '愛少女ポリアンナ物語' }
+        let(:number_in_season) { '' }
+        let(:with_season_title) { true }
+
+        it '作品タイトル+#動画順を返す' do
+          expect(full_title).to eq '愛少女ポリアンナ物語　#1'
+        end
+      end
+    end
+  end
 end
