93c791fe778b0a06f0609b6563d8783e50a5b9c6
masatoshi_moritsuka <yakiyaki_ikayaki@yahoo.co.jp>
2019-04-29 18:33:02 +0900

    Episodeのテストをリファクタリング

--- spec/models/episode_spec.rb
+++ spec/models/episode_spec.rb
@@ -13,93 +13,101 @@ RSpec.describe Episode, type: :model do
 
   describe '#full_title' do
     let(:episode) { create(:episode, :with_season, params) }
-    let(:params) { { title: title, number_in_season: number_in_season } }
+    let(:params) { { title: episode_title, number_in_season: number_in_season } }
     let(:full_title) { episode.full_title(with_season_title) }
 
-    context '作品タイトルと動画タイトルが同じかつ作品タイトル表示フラグがtrue' do
-      context '話数が空' do
-        let(:title) { '愛少女ポリアンナ物語' }
-        let(:number_in_season) { '' }
-        let(:with_season_title) { true }
+    context '作品タイトルと動画タイトルが同じ' do
+      context '作品タイトル表示フラグがtrue' do
+        context '話数が空' do
+          let(:episode_title) { '愛少女ポリアンナ物語' }
+          let(:number_in_season) { '' }
+          let(:with_season_title) { true }
 
-        it '作品タイトル+#動画順を返す' do
-          expect(full_title).to eq '愛少女ポリアンナ物語　#1'
+          it '作品タイトル+#動画順を返す' do
+            expect(full_title).to eq '愛少女ポリアンナ物語　#1'
+          end
         end
-      end
 
-      context '話数が空でない' do
-        let(:title) { '愛少女ポリアンナ物語' }
-        let(:number_in_season) { '第1話' }
-        let(:with_season_title) { true }
+        context '話数が空でない' do
+          let(:episode_title) { '愛少女ポリアンナ物語' }
+          let(:number_in_season) { '第1話' }
+          let(:with_season_title) { true }
 
-        it '作品タイトル+話数を返す' do
-          expect(full_title).to eq '愛少女ポリアンナ物語　第1話'
-        end
-      end
-    end
-
-    context '作品タイトルと動画タイトルが同じかつ作品タイトル表示フラグがfalse' do
-      context '話数が空' do
-        let(:title) { '愛少女ポリアンナ物語' }
-        let(:number_in_season) { '' }
-        let(:with_season_title) { false }
-
-        it '作品タイトル+#動画順を返す' do
-          expect(full_title).to eq '愛少女ポリアンナ物語　#1'
-        end
-      end
-
-      context '話数が空でない' do
-        let(:title) { '愛少女ポリアンナ物語' }
-        let(:number_in_season) { '第1話' }
-        let(:with_season_title) { false }
-
-        it '作品タイトル+話数を返す' do
-          expect(full_title).to eq '愛少女ポリアンナ物語　第1話'
+          it '作品タイトル+話数を返す' do
+            expect(full_title).to eq '愛少女ポリアンナ物語　第1話'
+          end
         end
       end
     end
 
-    context '作品タイトルと動画タイトルが異なるかつ作品タイトル表示フラグがtrue' do
-      context '話数が空' do
-        let(:title) { 'エピソード1' }
-        let(:number_in_season) { '' }
-        let(:with_season_title) { true }
+    context '作品タイトルと動画タイトルが同じ' do
+      context '作品タイトル表示フラグがfalse' do
+        context '話数が空' do
+          let(:episode_title) { '愛少女ポリアンナ物語' }
+          let(:number_in_season) { '' }
+          let(:with_season_title) { false }
 
-        it '作品タイトル+動画タイトルを返す' do
-          expect(full_title).to eq '愛少女ポリアンナ物語　エピソード1'
+          it '作品タイトル+#動画順を返す' do
+            expect(full_title).to eq '愛少女ポリアンナ物語　#1'
+          end
         end
-      end
 
-      context '話数が空でない' do
-        let(:title) { 'エピソード1' }
-        let(:number_in_season) { '第1話' }
-        let(:with_season_title) { true }
+        context '話数が空でない' do
+          let(:episode_title) { '愛少女ポリアンナ物語' }
+          let(:number_in_season) { '第1話' }
+          let(:with_season_title) { false }
 
-        it '作品タイトル+話数+動画タイトルを返す' do
-          expect(full_title).to eq '愛少女ポリアンナ物語　第1話　エピソード1'
+          it '作品タイトル+話数を返す' do
+            expect(full_title).to eq '愛少女ポリアンナ物語　第1話'
+          end
         end
       end
     end
 
-    context '作品タイトルと動画タイトルが異なるかつ作品タイトル表示フラグがfalse' do
-      context '話数が空' do
-        let(:title) { 'エピソード1' }
-        let(:number_in_season) { '' }
-        let(:with_season_title) { false }
+    context '作品タイトルと動画タイトルが異なる' do
+      context '作品タイトル表示フラグがtrue' do
+        context '話数が空' do
+          let(:episode_title) { 'エピソード1' }
+          let(:number_in_season) { '' }
+          let(:with_season_title) { true }
 
-        it '動画タイトルを返す' do
-          expect(full_title).to eq 'エピソード1'
+          it '作品タイトル+動画タイトルを返す' do
+            expect(full_title).to eq '愛少女ポリアンナ物語　エピソード1'
+          end
+        end
+
+        context '話数が空でない' do
+          let(:episode_title) { 'エピソード1' }
+          let(:number_in_season) { '第1話' }
+          let(:with_season_title) { true }
+
+          it '作品タイトル+話数+動画タイトルを返す' do
+            expect(full_title).to eq '愛少女ポリアンナ物語　第1話　エピソード1'
+          end
         end
       end
+    end
 
-      context '話数が空でない' do
-        let(:title) { 'エピソード1' }
-        let(:number_in_season) { '第1話' }
-        let(:with_season_title) { false }
+    context '作品タイトルと動画タイトルが異なる' do
+      context '作品タイトル表示フラグがfalse' do
+        context '話数が空' do
+          let(:episode_title) { 'エピソード1' }
+          let(:number_in_season) { '' }
+          let(:with_season_title) { false }
 
-        it '話数+動画タイトルを返す' do
-          expect(full_title).to eq '第1話　エピソード1'
+          it '動画タイトルを返す' do
+            expect(full_title).to eq 'エピソード1'
+          end
+        end
+
+        context '話数が空でない' do
+          let(:episode_title) { 'エピソード1' }
+          let(:number_in_season) { '第1話' }
+          let(:with_season_title) { false }
+
+          it '話数+動画タイトルを返す' do
+            expect(full_title).to eq '第1話　エピソード1'
+          end
         end
       end
     end
