db0a8d054c90ea6127a5473345f76b61399d14e3
masatoshi_moritsuka <yakiyaki_ikayaki@yahoo.co.jp>
2019-04-29 12:58:00 +0900

    Seasonのfactoryを追加

---
+++ spec/factories/seasons.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+FactoryBot.define do
+  factory :season do
+    title { '愛少女ポリアンナ物語' }
+    watchable { true }
+    thumbnail_url { 'https://dammy.com' }
+    outline { 'あらすじ' }
+    cast { '[キャスト]<br>ポリアンナ:堀江美都子' }
+    staff { '[スタッフ]<br>原作者:エレナ・ホグマン・ポーター' }
+    produced_year { 1986 }
+    copyright { '©NIPPON ANIMATION CO.,LTD.' }
+  end
+end
