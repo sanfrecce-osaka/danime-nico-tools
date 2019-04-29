82df1afc08496900abc746cff786433f438bc3bd
masatoshi_moritsuka <yakiyaki_ikayaki@yahoo.co.jp>
2019-04-29 11:03:25 +0900

    Form::SeasonSearch#split_keywordsのキーワードが2つかつキーワードが半角スペース区切りの場合のテストを追加

--- spec/models/form/season_search_spec.rb
+++ spec/models/form/season_search_spec.rb
@@ -6,13 +6,27 @@ RSpec.describe Form::SeasonSearch do
   let(:form) { Form::SeasonSearch.new(params) }
 
   describe '#split_keywords' do
+    let(:keywords) { form.split_keywords }
+
     context 'キーワードが1つ' do
       let(:params) { { keywords: 'マジンガーZ', keyword_type: :season_title } }
 
       it 'キーワードの配列を返す' do
-        keywords = form.split_keywords
         expect(keywords.length).to eq 1
         expect(keywords).to eq ['マジンガーZ']
+      end
+    end
+
+    context 'キーワードが2つ' do
+      let(:params) { { keywords: keywords_params, keyword_type: :season_title } }
+
+      context '半角スペース区切り' do
+        let(:keywords_params) { 'マジンガーZ 暗黒大将軍' }
+
+        it 'キーワードの配列を返す' do
+          expect(keywords.length).to eq 2
+          expect(keywords).to eq %w(マジンガーZ 暗黒大将軍)
+        end
       end
     end
   end
