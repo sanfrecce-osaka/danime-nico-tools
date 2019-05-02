8fb40c2bdb58fa4707c73c67364bbbcec696759c
masatoshi_moritsuka <yakiyaki_ikayaki@yahoo.co.jp>
2019-05-02 14:26:22 +0900

    SeasonHash.already_createdで全てのfixtureが読み込み元のpathを持っているかを検証するテストを追加

--- spec/hashes/season_hash_spec.rb
+++ spec/hashes/season_hash_spec.rb
@@ -9,5 +9,10 @@ RSpec.describe SeasonHash do
     it '作品の全てのfixtureを読み込んでいる' do
       expect(already_created.length).to eq 1
     end
+
+    it '全てのfixtureが読み込み元のpathを持っている' do
+      expect(already_created.all? { |season| season.has_key?(:file_path) }).to be_truthy
+      expect(already_created.map(&:file_path).all? { |path| path.include?('./spec/fixtures/seasons/') }).to be_truthy
+    end
   end
 end
