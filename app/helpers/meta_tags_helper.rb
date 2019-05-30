21c3bd52d875e261df7061b672fd094c8c05d887
masatoshi_moritsuka <yakiyaki_ikayaki@yahoo.co.jp>
2019-05-30 22:58:53 +0900

    meta_tags_helperにogpに関する設定を追加

--- app/helpers/meta_tags_helper.rb
+++ app/helpers/meta_tags_helper.rb
@@ -8,6 +8,21 @@ module MetaTagsHelper
       charset: 'utf-8',
       description: 'dアニメストア ニコニコ支店のための検索サイト。',
       viewport: 'width=device-width, initial-scale=1, shrink-to-fit=no',
+      og: {
+        title: :title,
+        type: 'website',
+        site_name: 'danime-nico-tools',
+        description: :description,
+        image: 'https://danime-nico-tools.herokuapp.com/ogp.png',
+        url: 'https://danime-nico-tools.herokuapp.com'
+      },
+      twitter: {
+        card: 'summary',
+        site: '@danime-nico-tools',
+        description: :description,
+        image: 'https://danime-nico-tools.herokuapp.com/ogp.png',
+        domain: 'https://danime-nico-tools.herokuapp.com'
+      }
     }
   end
 end
